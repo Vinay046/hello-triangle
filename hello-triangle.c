b931078dd9296a14ffc0798b1828e76b244fd4a3
Vinay Venkanagoud Patil <vinayvep@axis.com>
2024-07-12 20:51:35 +0200

    Hello triangle with GLSL ES 1.00
    Change-Id: I5f6f95c8b99effe0d38a79e853342cf8983ec293

--- hello-triangle.c
+++ hello-triangle.c
@@ -12,6 +12,7 @@
 #include <linux/input-event-codes.h>
 
 #include "xdg-shell-client-protocol.h"
+#include "glm_wrapper.h"
 
 #if defined(DEBUG)
 #define LOG(...) fprintf(stderr, __VA_ARGS__)
@@ -60,14 +61,12 @@ struct _escontext ESContext = {
 #define WINDOW_WIDTH 1920
 #define WINDOW_HEIGHT 1080
 
-
-
 bool program_alive;
 int32_t old_w, old_h;
 
-static void xdg_toplevel_handle_configure(void *data,
-        struct xdg_toplevel *xdg_toplevel, int32_t w, int32_t h,
-        struct wl_array *states) {
+static void xdg_toplevel_handle_configure(void *data __attribute__((unused)),
+        struct xdg_toplevel *xdg_toplevel __attribute__((unused)), int32_t w, int32_t h,
+        struct wl_array *states __attribute__((unused))) {
 
     if(w == 0 && h == 0) return;
 
@@ -80,8 +79,8 @@ static void xdg_toplevel_handle_configure(void *data,
     }
 }
 
-static void xdg_toplevel_handle_close(void *data,
-        struct xdg_toplevel *xdg_toplevel) {
+static void xdg_toplevel_handle_close(void *data __attribute__((unused)),
+        struct xdg_toplevel *xdg_toplevel __attribute__((unused))) {
     program_alive = false;
 }
 
@@ -90,7 +89,7 @@ struct xdg_toplevel_listener xdg_toplevel_listener = {
     .close = xdg_toplevel_handle_close,
 };
 
-static void xdg_surface_configure(void *data, struct xdg_surface *xdg_surface,
+static void xdg_surface_configure(void *data __attribute__((unused)), struct xdg_surface *xdg_surface,
         uint32_t serial) {
     xdg_surface_ack_configure(xdg_surface, serial);
 }
@@ -99,7 +98,7 @@ const struct xdg_surface_listener xdg_surface_listener = {
     .configure = xdg_surface_configure,
 };
 
-static void xdg_wm_base_ping(void *data, struct xdg_wm_base *xdg_wm_base,
+static void xdg_wm_base_ping(void *data __attribute__((unused)), struct xdg_wm_base *xdg_wm_base,
         uint32_t serial) {
     xdg_wm_base_pong(xdg_wm_base, serial);
 }
@@ -108,29 +107,29 @@ const struct xdg_wm_base_listener xdg_wm_base_listener = {
     .ping = xdg_wm_base_ping,
 };
 
-static void keyboard_handle_keymap(void *data, struct wl_keyboard *keyboard, uint32_t format, int fd, uint32_t size) {
+static void keyboard_handle_keymap(void *data __attribute__((unused)), struct wl_keyboard *keyboard __attribute__((unused)), uint32_t format __attribute__((unused)), int fd __attribute__((unused)), uint32_t size __attribute__((unused))) {
     // This function can be left empty
 }
 
-static void keyboard_handle_enter(void *data, struct wl_keyboard *keyboard, uint32_t serial, struct wl_surface *surface, struct wl_array *keys) {
+static void keyboard_handle_enter(void *data __attribute__((unused)), struct wl_keyboard *keyboard __attribute__((unused)), uint32_t serial __attribute__((unused)), struct wl_surface *surface __attribute__((unused)), struct wl_array *keys __attribute__((unused))) {
     // This function can be left empty
 }
 
-static void keyboard_handle_leave(void *data, struct wl_keyboard *keyboard, uint32_t serial, struct wl_surface *surface) {
+static void keyboard_handle_leave(void *data __attribute__((unused)), struct wl_keyboard *keyboard __attribute__((unused)), uint32_t serial __attribute__((unused)), struct wl_surface *surface __attribute__((unused))) {
     // This function can be left empty
 }
 
-static void keyboard_handle_key(void *data, struct wl_keyboard *keyboard, uint32_t serial, uint32_t time, uint32_t key, uint32_t state) {
+static void keyboard_handle_key(void *data __attribute__((unused)), struct wl_keyboard *keyboard __attribute__((unused)), uint32_t serial __attribute__((unused)), uint32_t time __attribute__((unused)), uint32_t key, uint32_t state) {
     if (state == WL_KEYBOARD_KEY_STATE_PRESSED && key == KEY_Q) {
         program_alive = false;
     }
 }
 
-static void keyboard_handle_modifiers(void *data, struct wl_keyboard *keyboard, uint32_t serial, uint32_t mods_depressed, uint32_t mods_latched, uint32_t mods_locked, uint32_t group) {
+static void keyboard_handle_modifiers(void *data __attribute__((unused)), struct wl_keyboard *keyboard __attribute__((unused)), uint32_t serial __attribute__((unused)), uint32_t mods_depressed __attribute__((unused)), uint32_t mods_latched __attribute__((unused)), uint32_t mods_locked __attribute__((unused)), uint32_t group __attribute__((unused))) {
     // This function can be left empty
 }
 
-static void keyboard_handle_repeat_info(void *data, struct wl_keyboard *keyboard, int32_t rate, int32_t delay) {
+static void keyboard_handle_repeat_info(void *data __attribute__((unused)), struct wl_keyboard *keyboard __attribute__((unused)), int32_t rate __attribute__((unused)), int32_t delay __attribute__((unused))) {
     // This function can be left empty
 }
 
@@ -143,7 +142,7 @@ static const struct wl_keyboard_listener keyboard_listener = {
     .repeat_info = keyboard_handle_repeat_info
 };
 
-static void seat_handle_capabilities(void *data, struct wl_seat *seat, uint32_t capabilities) {
+static void seat_handle_capabilities(void *data __attribute__((unused)), struct wl_seat *seat, uint32_t capabilities) {
     if (capabilities & WL_SEAT_CAPABILITY_KEYBOARD) {
         keyboard = wl_seat_get_keyboard(seat);
         wl_keyboard_add_listener(keyboard, &keyboard_listener, NULL);
@@ -155,7 +154,7 @@ static const struct wl_seat_listener seat_listener = {
     .name = NULL
 };
 
-void CreateNativeWindow(char *title, int width, int height) {
+void CreateNativeWindow(char *title __attribute__((unused)), int width, int height) {
     old_w = WINDOW_WIDTH;
     old_h = WINDOW_HEIGHT;
 
@@ -169,8 +168,10 @@ void CreateNativeWindow(char *title, int width, int height) {
     if (egl_window == EGL_NO_SURFACE) {
         LOG("No window !?\n");
         exit(1);
+    } else {
+        LOG("Window created !\n");
     }
-    else LOG("Window created !\n");
+
     ESContext.window_width = width;
     ESContext.window_height = height;
     ESContext.native_window = egl_window;
@@ -186,20 +187,21 @@ EGLBoolean CreateEGLContext () {
     EGLint fbAttribs[] = {
         EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
         EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
-        EGL_RED_SIZE,        8,
-        EGL_GREEN_SIZE,      8,
-        EGL_BLUE_SIZE,       8,
+        EGL_RED_SIZE, 8,
+        EGL_GREEN_SIZE, 8,
+        EGL_BLUE_SIZE, 8,
         EGL_NONE
     };
     EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE, EGL_NONE };
     EGLDisplay display = eglGetDisplay(ESContext.native_display);
+
     if (display == EGL_NO_DISPLAY) {
         LOG("No EGL Display...\n");
         return EGL_FALSE;
     }
 
     if (!eglInitialize(display, &majorVersion, &minorVersion)) {
-        LOG("No Initialisation...\n");
+        LOG("No Initialization...\n");
         return EGL_FALSE;
     }
 
@@ -245,15 +247,24 @@ GLuint program;
 GLuint vbo;
 GLint pos_location;
 
+
+// Vertex and Fragment shader sources
 const char *vertex_shader_source =
-    "attribute vec2 pos;\n"
+    "#version 100\n"
+    "attribute vec3 vPosition;\n"
+    "attribute vec3 vColor;\n"
+    "varying vec3 fColor;\n"
     "void main() {\n"
-    "    gl_Position = vec4(pos, 0.0, 1.0);\n"
+    "    gl_Position = vec4(vPosition, 1.0);\n"
+    "    fColor = vColor;\n"
     "}\n";
 
 const char *fragment_shader_source =
+    "#version 100\n"
+    "precision mediump float;\n"
+    "varying vec3 fColor;\n"
     "void main() {\n"
-    "    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "    gl_FragColor = vec4(fColor, 1.0);\n"
     "}\n";
 
 GLuint create_shader(const char *source, GLenum shader_type) {
@@ -283,16 +294,14 @@ void setup_graphics() {
         fprintf(stderr, "Error creating vertex shader\n");
         exit(1);
     }
-    LOG("Vertex shader created\n");
 
     GLuint fragment_shader = create_shader(fragment_shader_source, GL_FRAGMENT_SHADER);
     if (!fragment_shader) {
         fprintf(stderr, "Error creating fragment shader\n");
         exit(1);
     }
-    LOG("Fragment shader created\n");
 
-    program = glCreateProgram();
+    GLuint program = glCreateProgram();
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
     glLinkProgram(program);
@@ -314,34 +323,58 @@ void setup_graphics() {
 
     glUseProgram(program);
 
+    // Set up vertex data and buffers and configure vertex attributes
     GLfloat vertices[] = {
-        0.0f,  0.5f,
-       -0.5f, -0.5f,
-        0.5f, -0.5f
+        // Positions       // Colors
+         0.0f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // Top vertex (red)
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // Bottom-left vertex (green)
+         0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // Bottom-right vertex (blue)
     };
 
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    GLuint indices[] = {
+        0, 1, 2  // First triangle
+    };
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    pos_location = glGetAttribLocation(program, "pos");
-    glEnableVertexAttribArray(pos_location);
-    glVertexAttribPointer(pos_location, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    glBindVertexArray(VAO);
 }
 
 void draw() {
     glClearColor(0.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 }
 
 void RefreshWindow() {
     eglSwapBuffers(ESContext.display, ESContext.surface);
 }
 
-static void global_registry_handler(void *data, struct wl_registry *registry, uint32_t id,
-                                    const char *interface, uint32_t version) {
+static void global_registry_handler(void *data __attribute__((unused)), struct wl_registry *registry, uint32_t id,
+                                    const char *interface, uint32_t version __attribute__((unused))) {
     LOG("Got a registry event for %s id %d\n", interface, id);
     if (strcmp(interface, "wl_compositor") == 0)
         compositor = wl_registry_bind(registry, id, &wl_compositor_interface, 1);
@@ -354,7 +387,7 @@ static void global_registry_handler(void *data, struct wl_registry *registry, ui
     }
 }
 
-static void global_registry_remover(void *data, struct wl_registry *registry, uint32_t id) {
+static void global_registry_remover(void *data __attribute__((unused)), struct wl_registry *registry __attribute__((unused)), uint32_t id __attribute__((unused))) {
     LOG("Got a registry losing event for %d\n", id);
 }
 
@@ -402,8 +435,9 @@ int main() {
     if (surface == NULL) {
         LOG("No Compositor surface ! Yay....\n");
         exit(1);
+    } else {
+        LOG("Got a compositor surface !\n");
     }
-    else LOG("Got a compositor surface !\n");
 
     XDGSurface = xdg_wm_base_get_xdg_surface(XDGWMBase, surface);
     xdg_surface_add_listener(XDGSurface, &xdg_surface_listener, NULL);
@@ -420,7 +454,7 @@ int main() {
     }
 
     setup_graphics();
-
+    glEnable(GL_DEPTH_TEST);
     program_alive = true;
 
     while (program_alive) {
